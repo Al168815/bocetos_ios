95225b30c013cdcb32d33870a1fccd1bc4a43baa
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-09-18 10:36:53 -0600

    Citas

---
+++ boceto_1/boceto_1/Controlador/generador_de_citas.swift
@@ -0,0 +1,8 @@
+//
+//  generador_de_citas.swift
+//  boceto_1
+//
+//  Created by alumno on 9/18/24.
+//
+
+import Foundation
---
+++ boceto_1/boceto_1/Modelo/Cita.swift
@@ -0,0 +1,8 @@
+//
+//  Cita.swift
+//  boceto_1
+//
+//  Created by alumno on 9/18/24.
+//
+
+import Foundation
