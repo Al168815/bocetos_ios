f4549c6fd205236ce1f504c3d0917a631386b1e0
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-09-20 10:40:55 -0600

    pixel art

---
+++ boceto_1/boceto_1/Controlador_de_Visitas/ControlladorCita.swift
@@ -0,0 +1,18 @@
+//
+//  ControlladorCita.swift
+//  boceto_1
+//
+//  Created by alumno on 9/20/24.
+//
+
+import SwiftUI
+
+struct ControlladorCita: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlladorCita()
+}
